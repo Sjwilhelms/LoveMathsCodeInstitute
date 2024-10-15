c0404c02c63938c4e93f4bcc128e615e3cc034e6
Sam Williams <sjwilliams388@gmail.com>
2024-10-15 09:49:48 +0000

    add function skeleton

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,29 @@
+function runGame(){
+
+}
+function calculateCorrectAnswer(){
+
+}
+function checkAnswer(){
+
+}
+function incrementScore(){
+
+}
+function incrementWrongAnswer(){
+
+}
+function displayAdditionQuestion(){
+
+}
+function displaySubtractQuestion(){
+
+}
+function displayMultiplyQuestion(){
+
+}
+function displayDivisionQuestion(){
+
+}
+
 console.log("yoohoo");
