b645eafef42a02b49ce46481fb0263e7d0ac69c1
Sam Williams <sjwilliams388@gmail.com>
2024-10-15 12:53:18 +0000

    add UX tidy up

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,13 +11,24 @@ document.addEventListener("DOMContentLoaded", function () {
             }
         })
     }
+
+    document.getElementById("answer-box").addEventListener("keydown", function(event) {
+        if(event.key === "Enter"){
+            checkAnswer();
+        }
+    })
+
     runGame("addition");
-    
+
 })
 
 /** The main game loop, called when the script is first loaded and after the users answer has been processed */
 
 function runGame(gameType) {
+
+    document.getElementById("answer-box").value = "";
+    document.getElementById("answer-box").focus();
+
     // creates two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
@@ -97,7 +108,7 @@ function displayAdditionQuestion(operand1, operand2) {
 
 function displaySubtractQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand2 > operand1 ? operand2 : operand1;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "-";
 }
 
@@ -109,7 +120,7 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 function displayDivisionQuestion(operand1, operand2) {
     document.getElementById("operand1").textContent = operand1 > operand2 ? operand1 : operand2;
-    document.getElementById("operand2").textContent = operand2 > operand1 ? operand2 : operand1;
+    document.getElementById("operand2").textContent = operand1 > operand2 ? operand2 : operand1;
     document.getElementById("operator").textContent = "/";
 }
 
