ca742ff513f02ebb19ed9fad80557cd2f13c1e8b
Sam Williams <sjwilliams388@gmail.com>
2024-10-15 10:05:21 +0000

    add function to get the buttons and add event listeneres

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,55 @@
-function runGame(){
+// get the button elements adn add event listeners
+document.addEventListener("DOMContentLoaded", function(){
+    let buttons = document.getElementsByTagName("button");
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if(this.getAttribute("data-type") === "submit"){
+                alert("You clicked submit!");
+            }else{
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+/** The main game loop, called when the script is first loaded and after the users answer has been processed */
 
+function runGame(){
+    // creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
+
+runGame();
+
 function calculateCorrectAnswer(){
 
 }
+
 function checkAnswer(){
 
 }
+
 function incrementScore(){
 
 }
+
 function incrementWrongAnswer(){
 
 }
+
 function displayAdditionQuestion(){
 
 }
+
 function displaySubtractQuestion(){
 
 }
+
 function displayMultiplyQuestion(){
 
 }
+
 function displayDivisionQuestion(){
 
 }
