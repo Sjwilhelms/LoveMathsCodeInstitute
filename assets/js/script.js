e1043ce5db4c3369b6e350f603e1c77a731e1fd0
Sam Williams <sjwilliams388@gmail.com>
2024-10-15 10:27:29 +0000

    add code to check answer and respond to user

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(){
     for (let button of buttons){
         button.addEventListener("click", function(){
             if(this.getAttribute("data-type") === "submit"){
-                alert("You clicked submit!");
+                checkAnswer();
             }else{
                 let gameType = this.getAttribute("data-type");
                 runGame("gameType");
@@ -43,9 +43,17 @@ function calculateCorrectAnswer(){
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
 }
-
+/** checks the answer against the first element in the return caclualted correct answer array */
 function checkAnswer(){
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
 
+    if(isCorrect){
+        alert("Hey! You got it right 😍");
+    }else{
+        alert(`Wrong! You answered ${userAnswer}. The correct answer is ${calculatedAnswer[0]}!`);
+    }
 }
 
 function incrementScore(){
